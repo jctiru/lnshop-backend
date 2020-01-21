ab96654d5471fc8f310471c6fd9680ce6fbe08b1
jctiru <jonathanchristiantiru@gmail.com>
2020-01-21 15:47:26 +1000

    Initial commit

---
+++ lnshop/src/main/java/com/jctiru/lnshop/api/LnshopApplication.java
@@ -0,0 +1,13 @@
+package com.jctiru.lnshop.api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LnshopApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LnshopApplication.class, args);
+	}
+
+}
---
+++ lnshop/src/test/java/com/jctiru/lnshop/api/LnshopApplicationTests.java
@@ -0,0 +1,13 @@
+package com.jctiru.lnshop.api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LnshopApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
