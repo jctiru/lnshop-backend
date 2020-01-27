6d53606491ed6bc8138a53ffc2110bd882800323
jctiru <jonathanchristiantiru@gmail.com>
2020-01-27 23:32:57 +1000

    Add find by genre id in genre repository

--- lnshop/src/main/java/com/jctiru/lnshop/api/io/repository/GenreRepository.java
+++ lnshop/src/main/java/com/jctiru/lnshop/api/io/repository/GenreRepository.java
@@ -1,12 +1,16 @@
 package com.jctiru.lnshop.api.io.repository;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
 import com.jctiru.lnshop.api.io.entity.GenreEntity;
 
+@Repository
 public interface GenreRepository extends JpaRepository<GenreEntity, Long> {
 
 	GenreEntity findGenreByName(String name);
+
+	GenreEntity findByGenreId(String genreId);
 
 	boolean existsGenreByName(String name);
 
