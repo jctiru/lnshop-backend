12b813e9185a784aff8e45f4f242aaeea106da50
jctiru <jonathanchristiantiru@gmail.com>
2020-01-28 15:35:42 +1000

    Create genre response model

---
+++ lnshop/src/main/java/com/jctiru/lnshop/api/ui/model/response/GenreRest.java
@@ -0,0 +1,24 @@
+package com.jctiru.lnshop.api.ui.model.response;
+
+public class GenreRest {
+
+	private String genreId;
+	private String name;
+
+	public String getGenreId() {
+		return genreId;
+	}
+
+	public void setGenreId(String genreId) {
+		this.genreId = genreId;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+}
