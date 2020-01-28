151b4d0102fe468f8790abb34b2b490516632582
jctiru <jonathanchristiantiru@gmail.com>
2020-01-28 16:21:29 +1000

    Create bind exception handler

--- lnshop/src/main/java/com/jctiru/lnshop/api/exception/AppExceptionsHandler.java
+++ lnshop/src/main/java/com/jctiru/lnshop/api/exception/AppExceptionsHandler.java
@@ -7,6 +7,7 @@ import java.util.List;
 import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.validation.BindException;
 import org.springframework.validation.ObjectError;
 import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ControllerAdvice;
@@ -22,6 +23,21 @@ public class AppExceptionsHandler extends ResponseEntityExceptionHandler {
 	@Override
 	public final ResponseEntity<Object> handleMethodArgumentNotValid(
 			MethodArgumentNotValidException ex, HttpHeaders headers, HttpStatus status, WebRequest request) {
+		List<String> details = new ArrayList<>();
+
+		for (ObjectError error : ex.getBindingResult().getAllErrors()) {
+			details.add(error.getDefaultMessage());
+		}
+
+		ErrorResponse error = new ErrorResponse(HttpStatus.BAD_REQUEST, LocalDateTime.now(), "Validation Errors",
+				details);
+
+		return new ResponseEntity<>(error, HttpStatus.BAD_REQUEST);
+	}
+
+	@Override
+	public final ResponseEntity<Object> handleBindException(
+			BindException ex, HttpHeaders headers, HttpStatus status, WebRequest request) {
 		List<String> details = new ArrayList<>();
 
 		for (ObjectError error : ex.getBindingResult().getAllErrors()) {
