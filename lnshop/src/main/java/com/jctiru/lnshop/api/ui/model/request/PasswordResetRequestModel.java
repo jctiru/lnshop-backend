a32be58b4042d2c9b9822004aa10c4a6396609f5
jctiru <jonathanchristiantiru@gmail.com>
2020-03-18 22:25:05 +0800

    Create password reset request model

---
+++ lnshop/src/main/java/com/jctiru/lnshop/api/ui/model/request/PasswordResetRequestModel.java
@@ -0,0 +1,15 @@
+package com.jctiru.lnshop.api.ui.model.request;
+
+public class PasswordResetRequestModel {
+
+	private String email;
+
+	public String getEmail() {
+		return email;
+	}
+
+	public void setEmail(String email) {
+		this.email = email;
+	}
+
+}
