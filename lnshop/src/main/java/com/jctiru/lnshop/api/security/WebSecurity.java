b57a79b3cb9c24eb431af03a691bb6031fefcd6f
jctiru <jonathanchristiantiru@gmail.com>
2020-03-22 00:45:04 +0800

    Make password reset url endpoint public

--- lnshop/src/main/java/com/jctiru/lnshop/api/security/WebSecurity.java
+++ lnshop/src/main/java/com/jctiru/lnshop/api/security/WebSecurity.java
@@ -33,6 +33,7 @@ public class WebSecurity extends WebSecurityConfigurerAdapter {
 				.antMatchers(HttpMethod.POST, SecurityConstants.SIGN_UP_URL).permitAll()
 				.antMatchers(HttpMethod.GET, SecurityConstants.EMAIL_VERIFICATION_URL).permitAll()
 				.antMatchers(HttpMethod.POST, SecurityConstants.PASSWORD_RESET_REQUEST_URL).permitAll()
+				.antMatchers(HttpMethod.POST, SecurityConstants.PASSWORD_RESET_URL).permitAll()
 				.antMatchers(HttpMethod.POST, "/lightnovels/**").hasAuthority("ADMIN")
 				.antMatchers(HttpMethod.GET, "/lightnovels/**").permitAll()
 				.antMatchers(HttpMethod.GET, "/orders/admin").hasAuthority("ADMIN")
