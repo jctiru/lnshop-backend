11c2ac81a3feee7d2da750b04775f61e52b5612e
jctiru <jonathanchristiantiru@gmail.com>
2020-03-21 22:15:35 +0800

    Add validation in password reset model

--- lnshop/src/main/java/com/jctiru/lnshop/api/ui/model/request/PasswordResetModel.java
+++ lnshop/src/main/java/com/jctiru/lnshop/api/ui/model/request/PasswordResetModel.java
@@ -1,8 +1,15 @@
 package com.jctiru.lnshop.api.ui.model.request;
 
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.Size;
+
 public class PasswordResetModel {
 
+	@NotBlank(message = "Token must not be empty")
 	private String token;
+
+	@NotBlank(message = "Password must not be empty")
+	@Size(min = 6, message = "Password must be at least 6 characters")
 	private String password;
 
 	public String getToken() {
